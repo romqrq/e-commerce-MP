ed89b6efe7f2dea8598f88c3769483b54a8400e2
romqrq <rjaalbuquerque@gmail.com>
2020-05-18 14:15:45 +0000

    Added heroku to allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', ]
+ALLOWED_HOSTS = ['localhost', 'ecommerce-rom.herokuapp.com', ]
 
 
 # Application definition
